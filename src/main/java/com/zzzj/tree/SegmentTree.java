9e38126e7f0a693c8c9d5e6e9ce80426bc41150b
zzzj <1152092752@qq.com>
2023-08-24 23:27:29 +0800

    update SegmentTree

--- src/main/java/com/zzzj/tree/SegmentTree.java
+++ src/main/java/com/zzzj/tree/SegmentTree.java
@@ -1,5 +1,10 @@
 package com.zzzj.tree;
 
+import com.zzzj.leet.LeetUtils;
+import com.zzzj.util.ArrayUtil;
+
+import java.util.Arrays;
+
 /**
  * 线段树
  *
@@ -8,116 +13,249 @@ package com.zzzj.tree;
  */
 public class SegmentTree {
 
-    private int[] tree;
+    private final int N;
 
-    private int[] data;
+    private final int[] tree;
+
+    private final int[] lazy;
+
+    private final boolean[] changed;
+
+    private final int[] update;
 
     public SegmentTree(int[] arr) {
-        this.data = arr.clone();
-        // 完全二叉树的最大长度就是 4n
-        tree = new int[(arr.length << 2)];
-        // 构建线段树
-        buildSegmentTree(0, 0, arr.length - 1);
+        this.N = arr.length;
+        this.tree = new int[N << 2];
+        this.lazy = new int[N << 2];
+        this.changed = new boolean[N << 2];
+        this.update = new int[N << 2];
+        buildTree(arr, 1, N, 1);
     }
 
-    private static int getLeftChild(int index) {
-        return (index << 1) + 1;
+    public void update(int L, int R, int C) {
+        update(L + 1, R + 1, C, 1, N, 1);
     }
 
-    private static int getRightChild(int index) {
-        return (index << 1) + 2;
-    }
-
-    private void buildSegmentTree(int index, int rangeL, int rangeR) {
-        if (rangeL == rangeR) {
-            tree[index] = data[rangeL];
+    private void update(int L, int R, int C, int l, int r, int rt) {
+        if (L <= l && R >= r) {
+            changed[rt] = true;
+            update[rt] = C;
+            lazy[rt] = 0;
+            tree[rt] = (r - l + 1) * C;
             return;
         }
 
-        int mid = (rangeR + rangeL) / 2;
+        pushDown(l, r, rt);
 
-        int leftChild = getLeftChild(index);
-        int rightChild = getRightChild(index);
+        int mid = l + ((r - l) >> 1);
 
-        // 左边 rangeL ~ mid
-        buildSegmentTree(leftChild, rangeL, mid);
-        // 右边 mid + 1 ~ rangeR
-        buildSegmentTree(rightChild, mid + 1, rangeR);
+        if (L <= mid)
+            update(L, R, C, l, mid, leftChild(rt));
+        if (R > mid)
+            update(L, R, C, mid + 1, r, rightChild(rt));
 
-        // sum :
-        tree[index] = tree[leftChild] + tree[rightChild];
+        pushUp(rt);
     }
 
-    public void update(int index, int value) {
-        data[index] = value;
-        update(index, 0, 0, data.length - 1, value);
+    public void add(int L, int R, int C) {
+        add(L + 1, R + 1, C, 1, N, 1);
     }
 
-    private void update(int index, int treeIndex, int rangeL, int rangeR, int value) {
-        if (rangeL == index && rangeR == index) {
-            tree[treeIndex] = value;
+    private void add(int L, int R, int C, int l, int r, int rt) {
+        if (L <= l && R >= r) {
+            lazy[rt] += C;
+            tree[rt] += (r - l + 1) * C;
             return;
         }
 
-        int mid = (rangeR + rangeL) / 2;
+        pushDown(l, r, rt);
 
-        int leftChild = getLeftChild(treeIndex);
-        int rightChild = getRightChild(treeIndex);
+        int mid = l + ((r - l) >> 1);
 
-        // 左边
-        if (index <= mid) {
-            update(index, leftChild, rangeL, mid, value);
-            // 右边
-        } else {
-            update(index, rightChild, mid + 1, rangeR, value);
-        }
+        if (L <= mid)
+            add(L, R, C, l, mid, leftChild(rt));
+        if (R > mid)
+            add(L, R, C, mid + 1, r, rightChild(rt));
 
-        // 递归回来更新值
-        tree[treeIndex] = tree[leftChild] + tree[rightChild];
+        pushUp(rt);
     }
 
-    public int getRange(int searchL, int searchR) {
-        if (searchL < 0 || searchR >= data.length || searchL > searchR) {
-            return -1;
+    private void pushDown(int l, int r, int rt) {
+
+        int LC = leftChild(rt);
+        int RC = rightChild(rt);
+
+        if (changed[rt]) {
+            changed[rt] = false;
+            changed[LC] = true;
+            changed[RC] = true;
+
+            lazy[LC] = 0;
+            lazy[RC] = 0;
+
+            int mid = l + ((r - l) >> 1);
+            int leftChildCnt = mid - l + 1;
+            int rightChildCnt = r - mid;
+
+            update[LC] = update[rt];
+            update[RC] = update[rt];
+            tree[LC] = leftChildCnt * update[rt];
+            tree[RC] = rightChildCnt * update[rt];
+
+            update[rt] = 0;
         }
-        return getRange(0, 0, data.length - 1, searchL, searchR);
+
+        if (lazy[rt] != 0) {
+
+            int mid = l + ((r - l) >> 1);
+            int leftChildCnt = mid - l + 1;
+            int rightChildCnt = r - mid;
+
+            lazy[LC] += lazy[rt];
+            lazy[RC] += lazy[rt];
+            tree[LC] += leftChildCnt * lazy[rt];
+            tree[RC] += rightChildCnt * lazy[rt];
+            lazy[rt] = 0;
+        }
+
     }
 
-    private int getRange(int index, int rangeL, int rangeR, int searchL, int searchR) {
-        if (rangeL >= searchL && rangeR <= searchR) {
-            return tree[index];
-        }
-
-        int leftChild = getLeftChild(index);
-        int rightChild = getRightChild(index);
-
-        int leftValue = 0;
-        int rightValue = 0;
-
-        int mid = (rangeR + rangeL) / 2;
-
-        if (mid >= searchL) {
-            leftValue = getRange(leftChild, rangeL, mid, searchL, searchR);
-        }
-
-        if ((mid + 1) <= searchR) {
-            rightValue = getRange(rightChild, mid + 1, rangeR, searchL, searchR);
-        }
-
-        return leftValue + rightValue;
+    private void pushUp(int rt) {
+        tree[rt] = tree[leftChild(rt)] + tree[rightChild(rt)];
     }
 
-
-    public static void main(String[] args) {
-        int[] data = {1, 2, 3, 4, 5, 6, 7};
-
-        SegmentTree segmentTree = new SegmentTree(data);
-
-        System.out.println(segmentTree.getRange(2, 5));
-
-        segmentTree.update(3, 9);
-
-        System.out.println(segmentTree.getRange(2, 5));
+    public int query(int L, int R) {
+        return query(L + 1, R + 1, 1, N, 1);
     }
 
+    private int query(int L, int R, int l, int r, int rt) {
+        if (L <= l && R >= r) {
+            return tree[rt];
+        }
+
+        pushDown(l, r, rt);
+
+        int sum = 0;
+
+        int mid = l + ((r - l) >> 1);
+
+        if (L <= mid)
+            sum += query(L, R, l, mid, leftChild(rt));
+        if (R > mid)
+            sum += query(L, R, mid + 1, r, rightChild(rt));
+
+        return sum;
+    }
+
+    private void buildTree(int[] arr, int l, int r, int rt) {
+        if (l == r) {
+            tree[rt] = arr[l - 1];
+            return;
+        }
+
+        int mid = l + ((r - l) >> 1);
+
+        buildTree(arr, l, mid, leftChild(rt));
+        buildTree(arr, mid + 1, r, rightChild(rt));
+
+        pushUp(rt);
+    }
+
+    private int leftChild(int rt) {
+        return rt << 1;
+    }
+
+    private int rightChild(int rt) {
+        return rt << 1 | 1;
+    }
+
+    private static class TreeChecker {
+
+        private final int[] arr;
+
+        public TreeChecker(int[] arr) {
+            this.arr = arr;
+        }
+
+        public void add(int L, int R, int C) {
+            for (int i = L; i <= R; i++) {
+                arr[i] += C;
+            }
+        }
+
+        public void update(int L, int R, int C) {
+            for (int i = L; i <= R; i++) {
+                arr[i] = C;
+            }
+        }
+
+        public int query(int L, int R) {
+            int sum = 0;
+            for (int i = L; i <= R; i++) {
+                sum += arr[i];
+            }
+            return sum;
+        }
+
+        public static void main(String[] args) {
+
+            int LOOP_TIMES = 10000;
+
+            for (int i = 0; i < LOOP_TIMES; i++) {
+
+                int OP_TIMES = 1000;
+
+                int N = 1000;
+
+                int L = 1;
+
+                int R = 100;
+
+                int[] arr1 = ArrayUtil.generateArray(N, L, R);
+
+                int[] arr2 = Arrays.copyOfRange(arr1, 0, arr1.length);
+
+                SegmentTree segmentTree = new SegmentTree(arr1);
+
+                TreeChecker checker = new TreeChecker(arr2);
+
+                for (int j = 0; j < OP_TIMES; j++) {
+                    int op = LeetUtils.random.nextInt(3);
+
+                    int bounds1 = LeetUtils.random.nextInt(arr1.length);
+                    int bounds2 = LeetUtils.random.nextInt(arr1.length);
+
+                    int left = Math.min(bounds1, bounds2);
+                    int right = Math.max(bounds1, bounds2);
+
+                    int C = LeetUtils.random.nextInt(R) + L;
+
+                    switch (op) {
+                        case 0:
+                            segmentTree.add(left, right, C);
+                            checker.add(left, right, C);
+                            break;
+                        case 1:
+                            segmentTree.update(left, right, C);
+                            checker.update(left, right, C);
+                            break;
+                        case 2:
+                        default:
+                            int r1 = segmentTree.query(left, right);
+
+                            int r2 = checker.query(left, right);
+
+                            if (r1 != r2) {
+                                System.out.println("Error");
+                                return;
+                            }
+                            break;
+                    }
+                }
+
+            }
+
+            System.out.println("Ok~");
+        }
+    }
 }
